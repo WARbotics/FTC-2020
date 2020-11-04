90344c48d9227a7141d173a642b32d3e98282f2f
Victor Henriksson <vhenrixon@gmail.com>
2020-11-04 15:23:51 -0500

    WIP

---
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 37;
+  public static final String VERSION_NAME = "5.5";
+  // Fields from default config.
+  public static final String BUILD_TIME = "2020-10-28T16:10:39.850-0400";
+}
